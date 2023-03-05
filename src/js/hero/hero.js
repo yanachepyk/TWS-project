21469a4197f5b997808224471d83ce32dc2b0de1
Anastasiia <schili@outlook.com>
2023-03-05 19:31:07 +0200

    brought the listener to the common father

--- src/js/hero/hero.js
+++ src/js/hero/hero.js
@@ -2,14 +2,15 @@ const checkbox = document.querySelector('.check-box');
 const toChoose = document.querySelector('.to-choose');
 const letters = document.querySelectorAll('.js-letter');
 
-checkbox.addEventListener('click', () => {
-  toChoose.classList.toggle('to-choose--visible');
+toChoose.addEventListener('click', function (event) {
+  const target = event.target.closest('.js-letter');
+  if (target) {
+    event.preventDefault();
+    checkbox.childNodes[0].nodeValue = target.value;
+    toChoose.classList.remove('to-choose--visible');
+  }
 });
 
-letters.forEach(letter => {
-  letter.addEventListener('click', function (event) {
-    event.preventDefault();
-    checkbox.childNodes[0].nodeValue = event.target.value;
-    toChoose.classList.remove('to-choose--visible');
-  });
+checkbox.addEventListener('click', () => {
+  toChoose.classList.toggle('to-choose--visible');
 });
