19b73c886ac2e5073adc039ef49db52cf9e7b4ca
yanachepyk <yanapgp@gmail.com>
2023-03-06 21:24:26 +0200

    general changes

--- src/js/hero/listenerForLetter.js
+++ src/js/hero/listenerForLetter.js
@@ -1,23 +1,7 @@
 import { handleCocktailsSearch } from '../shared/handle-cocktails-search';
 
 const heroBox = document.querySelector('.hero__box');
-
-heroBox.addEventListener('click', event => {
-  if (event.target.classList.contains('js-letter')) {
-    const searchQuery = event.target.value.trim();
-    handleCocktailsSearch({
-      target: { nodeName: 'BUTTON', value: searchQuery },
-    });
-  }
-});
-
 const toChoose = document.querySelector('.to-choose');
 
-toChoose.addEventListener('click', event => {
-  if (event.target.classList.contains('js-letter')) {
-    const searchQuery = event.target.value.trim();
-    handleCocktailsSearch({
-      target: { nodeName: 'BUTTON', value: searchQuery },
-    });
-  }
-});
+heroBox.addEventListener('click', handleCocktailsSearch);
+toChoose.addEventListener('click', handleCocktailsSearch);
