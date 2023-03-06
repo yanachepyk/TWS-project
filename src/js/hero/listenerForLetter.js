2b07ac236d125bb5b179d731761d71d7fd77f36c
Anastasiia <schili@outlook.com>
2023-03-06 17:05:16 +0200

    commit listeners

---
+++ src/js/hero/listenerForLetter.js
@@ -0,0 +1,24 @@
+import { handleCocktailsSearch } from '../shared/handle-cocktails-search';
+
+const heroBox = document.querySelector('.hero__box');
+
+heroBox.addEventListener('click', event => {
+  if (event.target.classList.contains('js-letter')) {
+    const searchQuery = event.target.value.trim();
+    handleCocktailsSearch({
+      target: { nodeName: 'BUTTON', value: searchQuery },
+    });
+    console.log('you click for letter');
+  }
+});
+
+const toChoose = document.querySelector('.to-choose');
+
+toChoose.addEventListener('click', event => {
+  if (event.target.classList.contains('js-letter')) {
+    const searchQuery = event.target.value.trim();
+    handleCocktailsSearch({
+      target: { nodeName: 'BUTTON', value: searchQuery },
+    });
+  }
+});
