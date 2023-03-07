606069aacaf2cb19cb984d796fadef419c5d5e0a
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-03-07 19:07:23 +0300

    modal ingridient

--- src/js/shared/handle-Learn-More-Btn-Click.js
+++ src/js/shared/handle-Learn-More-Btn-Click.js
@@ -1,21 +1,27 @@
-function handleLearnMoreBtnClick(e) {
+export function handleLearnMoreBtnClick(e) {
   e.preventDefault();
   if (e.target.nodeName !== 'BUTTON') return;
   else {
     const modalWindow = document.querySelector('[data-modal-cocktails]');
     const selectedCocktail = e.target.dataset.id;
     getCocktailsDetailsById(selectedCocktail).then(({ drinks }) => {
+      const ingredientsMeasure = [];
       const ingredients = [];
       for (let ingr in drinks[0]) {
         if (ingr.includes('strIngredient') && ingr !== null)
           ingredients.push(ingr);
       }
-      const listOfIngredients = ingredients.map(
-        item => ` <li class="modal-cocktails__item">
-      ✶ ${item}
+      for (let measure in drinks[0]) {
+        if (ingr.includes('strMeasure') && ingr !== null)
+          ingredientsMeasure.push(measure);
+      }
+      let listMarkup = '';
+      for (let i = 0; i < ingredients.length; i += 1) {
+        listMarkup = `<li class="modal-cocktails__item">
+      ✶ ${ingredientsMeasure[i]}<span class="ingredient" name='${ingredients[i]}'>${ingredients[i]}</span>
       <a class="modal-cocktails__link" href="#">Ingridient</a>
-    </li>`
-      );
+    </li>`;
+      }
       const markup = `<h1 class="modal-cocktails__title">${drinks[0].strDrink}</h1>
       <h2 class="modal-cocktails__subtitle">INSTRACTION:</h2>
       <p class="modal-cocktails__descrip">
@@ -31,9 +37,10 @@ function handleLearnMoreBtnClick(e) {
       <h2 class="modal-cocktails__conteine">INGREDIENTS</h2>
       <p class="modal-cocktails__per">Per cocktail</p>
       <ul class="modal-cocktails__list">
-      ${listOfIngredients}
+      ${listMarkup}
       </ul>`;
+      modalWindow.insertAdjacentHTML('afterbegin', markup);
+      modalWindow.classList.remove('hidden');
     });
   }
-  modalWindow.insertAdjacentHTML('afterbegin', markup);
 }
