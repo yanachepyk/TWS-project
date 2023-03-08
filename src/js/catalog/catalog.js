68dea91094bb774f00170fadabc33971c8ec3c9d
yanachepyk <yanapgp@gmail.com>
2023-03-08 11:31:04 +0200

    fix issue

--- src/js/catalog/catalog.js
+++ src/js/catalog/catalog.js
@@ -1,7 +1,7 @@
 import { getRandomCocktail} from '../shared/api-service';
 import { getCocktailsAmountPerPage } from '../shared/get-cocktails-amount-per-page';
 import { createCocktailsMarkup } from './create-cocktails-markup';
-import { Paginator } from './pagination/paginator';
+// import { Paginator } from './pagination/paginator';
 
 function getRandomCoctails(amount) {
   return Array(amount)
@@ -23,11 +23,11 @@ const refs = {
   paginator: document.querySelector('.paginator')
 };
 
-export const catalogPaginator = new Paginator({
-  selector: '.paginator',
-  drawMarkup: (cocktails) => {
-    refs.list.innerHTML = createCocktailsMarkup(cocktails);
-  }
-});
+// export const catalogPaginator = new Paginator({
+//   selector: '.paginator',
+//   drawMarkup: (cocktails) => {
+//     refs.list.innerHTML = createCocktailsMarkup(cocktails);
+//   }
+// });
 
 showInitialCoctails();
