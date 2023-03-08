86ec6d9e05a8a5faf36686f3fd315fe63c914367
Anastasiia <schili@outlook.com>
2023-03-08 17:53:01 +0200

    changes list-letters + page title

--- src/js/catalog/handle-cocktails-click-action.js
+++ src/js/catalog/handle-cocktails-click-action.js
@@ -1,16 +1,16 @@
-import { handleLearnMoreBtnClick } from "../shared/handle-Learn-More-Btn-Click";
+import { handleLearnMoreBtnClick } from '../shared/handle-Learn-More-Btn-Click';
 
 export function handleCocktailClickAction(event) {
-    if (event.target.nodeName !== 'BUTTON') {
-        return;
-    }
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
 
-    const action = event.target.dataset.action;
-    const cocktailId = event.target.dataset.id;
+  const action = event.target.dataset.action;
+  const cocktailId = event.target.dataset.id;
 
-    if (action === 'learn_more') {
-        handleLearnMoreBtnClick(cocktailId);
-    } else if (action === 'favorite') {
-        // провіряємо чи є у LocalStorage ("улюблених") айді цього коктейлю, та якщо ні - добавляємо
-    }
+  if (action === 'learn_more') {
+    handleLearnMoreBtnClick(cocktailId);
+  } else if (action === 'favorite') {
+    // провіряємо чи є у LocalStorage ("улюблених") айді цього коктейлю, та якщо ні - добавляємо
+  }
 }
--- src/js/hero/listenerForLetter.js
+++ src/js/hero/listenerForLetter.js
@@ -3,5 +3,5 @@ import { handleCocktailsSearch } from '../shared/handle-cocktails-search';
 const heroBox = document.querySelector('.hero__box');
 const toChoose = document.querySelector('.to-choose');
 
-// heroBox.addEventListener('click', handleCocktailsSearch);
-// toChoose.addEventListener('click', handleCocktailsSearch);
+heroBox.addEventListener('click', handleCocktailsSearch);
+toChoose.addEventListener('click', handleCocktailsSearch);
