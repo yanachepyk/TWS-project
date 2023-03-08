228e9835c1ef4f8f7093ef00b3934f6802f5de09
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-03-08 12:31:49 +0300

    modal window

--- src/js/shared/listener-btn-learnmore.js
+++ src/js/shared/listener-btn-learnmore.js
@@ -1,4 +1,6 @@
-import { handleLearnMoreBtnClick } from './handle-Learn-More-Btn-Click';
-
+import '../modal-ingredients/modal-ingredients';
+import { handleCocktailClickAction } from '../catalog/handle-cocktails-click-action';
+import { handleIngredientModalOpen } from './handle-ingredient-modal-open';
 const catalogList = document.querySelector('.catalog__list');
-catalogList.addEventListener('click', handleLearnMoreBtnClick);
+
+catalogList.addEventListener('click', handleCocktailClickAction);
