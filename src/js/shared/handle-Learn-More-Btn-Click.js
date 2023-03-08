863e603d3f26f425211631e495970ae93245b87b
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-03-08 13:54:37 +0300

    add

--- src/js/shared/handle-Learn-More-Btn-Click.js
+++ src/js/shared/handle-Learn-More-Btn-Click.js
@@ -1,7 +1,5 @@
 import { getCocktailsDetailsById } from './api-service';
 
-
-
 export function handleLearnMoreBtnClick(selectedCocktail) {
   const modalWindow = document.querySelector('[data-modal-cocktails]');
   const backdrop = document.querySelector('.backdrop-cocktails');
@@ -18,17 +16,17 @@ export function handleLearnMoreBtnClick(selectedCocktail) {
       }
     }
 
-
     for (let measure in drinks[0]) {
       if (measure.includes('strMeasure') && drinks[0][measure] !== null) {
         ingredientsMeasure.push(drinks[0][measure]);
       }
-    
     }
 
     for (let i = 0; i < ingredients.length; i += 1) {
       listMarkup.push(`<li class="modal-cocktails__item">
-      ✶ ${ingredientsMeasure[i] || ''}<span class="ingredient" name='${ingredients[i]}'>${ingredients[i]}</span>
+      ✶ ${ingredientsMeasure[i] || ''}<span class="ingredient" name='${
+        ingredients[i]
+      }'>${ingredients[i]}</span>
       <a class="modal-cocktails__link" href="#">Ingridient</a>
     </li>`);
     }
@@ -51,9 +49,9 @@ export function handleLearnMoreBtnClick(selectedCocktail) {
   ${list}
       </ul>`;
 
-    modalWindow.innerHTML =  markup;
+    modalWindow.innerHTML = markup;
 
     backdrop.classList.remove('hidden');
+    document.body.style.overflow = 'hidden';
   });
 }
-
