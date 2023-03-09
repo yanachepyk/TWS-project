880626b16b9b1cbd7cf0ad17cf057062ec28518e
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-03-09 12:26:47 +0300

    header scroll

--- src/js/header/header-form.js
+++ src/js/header/header-form.js
@@ -2,6 +2,15 @@ import { handleCocktailsSearch } from '../shared/handle-cocktails-search';
 
 const searchForm = document.querySelector('.header__search-form');
 const searchFormModal = document.querySelector('.header__search-form-modal');
+const header = document.querySelector('.header');
 
 searchForm.addEventListener('submit', handleCocktailsSearch);
 searchFormModal.addEventListener('submit', handleCocktailsSearch);
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
